95809e76b46089987706e242f57269b4d32f932f
brandonlafrantz <clear>
2021-02-25 00:50:39 -0600

    Sign Up Sign In update

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app'
+import 'firebase/firebase-firestore'
+
+const firestore = firebase.firestore();
+
+firestore.collection('Users').doc('kFvOw0jo7CSELHnLqeux').collection('cartItems').doc('4szpAbqumbLn9ZRiRwmf');
+firestore.doc('Users/kFvOw0jo7CSELHnLqeux/cartItems/szpAbqumbLn9ZRiRwmf');
+firestore.collection('Users/kFvOw0jo7CSELHnLqeux/cartItems');
